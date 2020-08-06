0932acf8237475675c2481df00761f42c223ed8d
Lucas de Almeida <lucasde.almeida@hotmail.comgit config --global user.email lucasde.almeida@hotmail.comgit config --global user.email lucasde.almeida@hotmail.com>
2020-08-06 20:11:31 -0300

    first commit

---
+++ lib/entities/user.dart
@@ -0,0 +1,10 @@
+class User {
+  String nome;
+  int registro;
+  int cpf;
+  String email;
+  int turno;
+  DateTime nascimento;
+  String telefone;
+  String localTrabalho;
+}
---
+++ lib/screens/login_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var now = new DateTime.now();
+  final formatter = new DateFormat('dd-MM-yyyy hh:mm');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.white,
+                    child: Text(
+                      formatter.format(now),
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                      ),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                            labelText: 'Registro',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                            )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Senha',
+                            labelStyle: TextStyle(
+                              color: Colors.black,
+                            )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            child: RaisedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Esqueci a senha',
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: RaisedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Entrar ',
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
